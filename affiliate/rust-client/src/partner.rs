677980c418893c8ede86281c92e52cc2fb36d4b8
andrewsource147 <31321699+andrewsource147@users.noreply.github.com>
2023-11-17 09:15:29 +0700

    init user permissionless (#12)

--- affiliate/rust-client/src/partner.rs
+++ affiliate/rust-client/src/partner.rs
@@ -44,6 +44,45 @@ pub fn init_user(
     Ok(())
 }
 
+pub fn init_user_permissionless(
+    program_client: &anchor_client::Program,
+    vault: Pubkey,
+    partner: String,
+) -> Result<()> {
+    let partner = Pubkey::from_str(&partner).unwrap();
+
+    let vault_state: mercurial_vault::state::Vault = program_client.account(vault)?;
+    let token_mint = vault_state.token_mint;
+    let partner_token = get_or_create_ata(program_client, token_mint, partner)?;
+    let (partner, _nonce) =
+        Pubkey::find_program_address(&[vault.as_ref(), partner_token.as_ref()], &affiliate::id());
+    println!("{} {}", partner, partner_token);
+    // return Ok(());
+    // check whether partner is existed
+    let _partner_state: affiliate::Partner = program_client.account(partner)?;
+
+    let (user, _nonce) = Pubkey::find_program_address(
+        &[partner.as_ref(), program_client.payer().as_ref()],
+        &affiliate::id(),
+    );
+
+    let builder = program_client
+        .request()
+        .accounts(affiliate::accounts::InitUserPermissionless {
+            user,
+            partner,
+            owner: program_client.payer(),
+            payer: program_client.payer(),
+            system_program: system_program::id(),
+        })
+        .args(affiliate::instruction::InitUserPermissionless {});
+
+    let signature = builder.send()?;
+    println!("{}", signature);
+
+    Ok(())
+}
+
 pub fn view_partner(
     program_client: &anchor_client::Program,
     vault: Pubkey,
